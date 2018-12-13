20033c6d071cd86ba1b8f6e201024f7e044d9ce9
Tshiamo <tshiamobhuda@gmail.com>
2018-12-13 11:10:24 +0200

    [day2] Part1 of day 2

--- src/day2/part1.php
+++ src/day2/part1.php
@@ -2,4 +2,20 @@
 
 declare(strict_types=1);
 
-$input = getInput();
+$input = explode(PHP_EOL, getInput());
+$twos = 0;
+$threes = 0;
+
+foreach ($input as $value) {
+    $id = array_count_values(str_split($value));
+
+    if (array_search(2, $id)) {
+        $twos++;
+    }
+
+    if (array_search(3, $id)) {
+        $threes++;
+    }
+}
+
+return $twos * $threes;
--- src/day2/test.php
+++ src/day2/test.php
@@ -2,7 +2,9 @@
 
 return [
     //part 1
-    [],
+    [
+        'abcdef, bababc, abbcde, abcccd, aabcdd, abcdee, ababab' => 12,
+    ],
 
     // part 2
     []
